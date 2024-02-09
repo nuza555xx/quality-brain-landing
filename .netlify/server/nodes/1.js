97f36e84915f63a4a609fb3041c3a6fac468258c
panuwat.c <panuwat.c@deftdev.tech>
2024-02-09 16:08:51 +0700

    init

---
+++ .netlify/server/nodes/1.js
@@ -0,0 +1,8 @@
+
+
+export const index = 1;
+let component_cache;
+export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
+export const imports = ["_app/immutable/nodes/1.Ztrpsw1g.js","_app/immutable/chunks/scheduler.zMJaRgub.js","_app/immutable/chunks/index.ZLPrqq-8.js","_app/immutable/chunks/entry.YQEPppV5.js"];
+export const stylesheets = [];
+export const fonts = [];
